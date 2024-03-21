2f5d70d11ed6ca22535aeb0ff8ad0fb14fdca4d6
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-03-21 11:19:00 +0200

    /api/invalid-access route is registered and accessDenied of AuthController is assigned to handel the GET request.

--- ci4/myapp/app/Config/Routes.php
+++ ci4/myapp/app/Config/Routes.php
@@ -12,11 +12,13 @@ service('auth')->routes($routes);
 // api routes
 
 $routes->group('api', ['namespace' => 'App\Controllers\Api'], function ($routes) {
+    // route for invalid access token
+    $routes->get('invalid-access', 'AuthController::accessDenied');
+    // routes for register, login, logout
     $routes->post('register', 'AuthController::register');
     $routes->post('login', 'AuthController::login');
-    $routes->get('profile', 'AuthController::profile');
+    $routes->get('profile', 'AuthController::profile', ['filter' => 'authFilter']);
     $routes->get('logout', 'AuthController::logout');
-
     // project routes
     $routes->post('add-project', 'ProjectController::addProject');
     $routes->get('get-projects', 'ProjectController::getProjects');
