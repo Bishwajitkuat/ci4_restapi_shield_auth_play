58edaf27674ebb1e5033329afc80ae8f7871daa1
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-03-21 10:40:03 +0200

    routes, and controller are registered.

--- ci4/myapp/app/Config/Routes.php
+++ ci4/myapp/app/Config/Routes.php
@@ -8,3 +8,17 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 service('auth')->routes($routes);
+
+// api routes
+
+$routes->group('api', ['namespace' => 'App\Controllers\Api'], function ($routes) {
+    $routes->post('register', 'AuthController::register');
+    $routes->post('login', 'AuthController::login');
+    $routes->get('profile', 'AuthController::profile');
+    $routes->get('logout', 'AuthController::logout');
+
+    // project routes
+    $routes->post('add-project', 'ProjectController::addProject');
+    $routes->get('get-projects', 'ProjectController::getProjects');
+    $routes->delete('delete-project/(:num)', 'ProjectController::deleteProject/$1');
+});
