9c63f92164ebabbc363ffa647f393209b83dff99
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-03-21 10:47:17 +0200

    ProjectController template is generated.

--- ci4/myapp/app/Controllers/Api/ProjectController.php
+++ ci4/myapp/app/Controllers/Api/ProjectController.php
@@ -6,5 +6,24 @@ use CodeIgniter\RESTful\ResourceController;
 
 class ProjectController extends ResourceController
 {
+    // post
+    public function addProject()
+    {
+        // insert project to db
+        // token to identify user
+    }
 
+    // get
+    public function getProjects()
+    {
+        // returns project belongs to this user
+        // token to identify user
+    }
+
+    // delete
+    public function deleteProject($id)
+    {
+        // delete project
+        // token to  user
+    }
 }
