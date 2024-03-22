1532707208c4c7549df0f97147289cf0b6abb292
Bishwajitkuat <bishwajitkuat@gmail.com>
2024-03-22 15:10:21 +0200

    some fields added to users table.

--- ci4/myapp/app/Database/Migrations/2024-03-22-125428_AddInfoToUsersTable.php
+++ ci4/myapp/app/Database/Migrations/2024-03-22-125428_AddInfoToUsersTable.php
@@ -2,17 +2,58 @@
 
 namespace App\Database\Migrations;
 
+use CodeIgniter\Database\Forge;
 use CodeIgniter\Database\Migration;
 
 class AddInfoToUsersTable extends Migration
 {
+    /**
+     * @var string[]
+     */
+    private array $tables;
+
+    public function __construct(?Forge $forge = null)
+    {
+        parent::__construct($forge);
+
+        /** @var \Config\Auth $authConfig */
+        $authConfig = config('Auth');
+        $this->tables = $authConfig->tables;
+    }
+
     public function up()
     {
-        //
+        $fields = [
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 99,
+                'null' => false,
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 99,
+                'null' => false,
+            ],
+            'mobile' => [
+                'type' => 'VARCHAR',
+                'constraint' => 99,
+                'null' => false,
+            ],
+            'address' => [
+                'type' => 'VARCHAR',
+                'constraint' => 500,
+                'null' => false,
+            ],
+
+        ];
+        $this->forge->addColumn($this->tables['users'], $fields);
     }
 
     public function down()
     {
-        //
+        $fields = [
+            'name', 'email', 'mobile', 'address',
+        ];
+        $this->forge->dropColumn($this->tables['users'], $fields);
     }
 }
